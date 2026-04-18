9ae8fad810b6d7737b0a68235155228fce36c3e7
zrq1351 <320401891@qq.com>
2026-04-19 02:55:09 +0800

    chore(deps): 更新 Cargo.toml 中的依赖项
    - 升级了项目依赖库到最新版本
    - 移除了不再使用的依赖项
    - 更新了依赖项的版本约束
    - 重新生成了 Cargo.lock 文件以同步依赖关系

--- test_vue.js
+++
@@ -1,8 +0,0 @@
-const { compileTemplate } = require('@vue/compiler-sfc');
-const code = `
-<template>
-  <div v-bind="containerProps" @scroll="handleScroll"></div>
-</template>
-`;
-const result = compileTemplate({ source: code, id: 'test' });
-console.log(result.code);
