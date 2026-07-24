bdacaca789ba950dc1e2d40f65175947d8a61eb3
zrq1351 <vzfzhong@gmail.com>
2026-07-24 23:59:13 +0800

    fix(clipboard): card height 100%, scroll-track padding provides equal top/bottom spacing

--- src/pages/clipboard/components/ClipboardList.vue
+++ src/pages/clipboard/components/ClipboardList.vue
@@ -189,7 +189,7 @@ defineExpose({contentRef})
   flex-direction: column;
   vertical-align: top;
   width: 260px;
-  height: calc(100% - 40px);
+  height: 100%;
   white-space: normal;
   margin-right: 10px;
   background: rgba(255, 255, 255, 0.04);
